0ae289041d96b99ee9f377a7848285aeb1a84d0d
Pierre M <pierre@gbiloba.org>
2021-01-25 14:35:16 +0100

    User & Role

---
+++ src/model/User.java
@@ -0,0 +1,38 @@
+package model;
+
+public class User {
+
+	private String name;
+	private String password;
+	private Role role;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	public User() {
+		this.role = Role.NORMAL;
+	}
+	
+	public User(String name, String password, Role role) {
+		this.name = name;
+		this.password = password;
+		this.role = role;
+	}
+	
+}
