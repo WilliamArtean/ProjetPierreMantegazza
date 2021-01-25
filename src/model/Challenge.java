5e5ed096972349cfcfb3839edd78ea22d881226f
Pierre M <pierre@gbiloba.org>
2021-01-25 15:04:05 +0100

    Expanded challenge classes

--- src/model/Challenge.java
+++ src/model/Challenge.java
@@ -6,5 +6,25 @@ public abstract class Challenge {
 	
 	protected List<Checkpoint> checkpoints;
 	protected List<Segment> segments;
+	protected String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Checkpoint> getCheckpoints() {
+		return checkpoints;
+	}
+	public void addCheckpoints(Checkpoint checkpoint) {
+		this.checkpoints.add(checkpoint);
+	}
+	public List<Segment> getSegments() {
+		return segments;
+	}
+	public void addSegments(Segment segment) {
+		this.segments.add(segment);
+	}
 	
 }
--- src/model/Obstacle.java
+++ src/model/Obstacle.java
@@ -2,4 +2,14 @@ package model;
 
 public class Obstacle {
 
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
