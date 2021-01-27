a54237e2deaa091e0267456d17cc18bd01ca33e8
Pierre M <pierre@gbiloba.org>
2021-01-27 16:48:16 +0100

    JPAified project, added User to persistence.xml

--- src/model/User.java
+++ src/model/User.java
@@ -1,14 +1,22 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.ws.rs.Path;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@Entity
 @Table
 @Path("/user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
 	@JsonProperty
 	private String id;
 	@JsonProperty
